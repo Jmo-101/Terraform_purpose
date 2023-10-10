27cc6fd4018238030adebd12023a03e721f618aa
Jorge Molina <138607757+Jmo-101@users.noreply.github.com>
2023-10-10 14:00:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,18 @@ provider "aws" {
 }
 
 # create instance
-resource "aws_instance" "web_server01" {
-  ami = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web_ssh.id]
+#resource "aws_instance" "web_server01" {
+  #ami = "ami-053b0d53c279acc90"
+#select an instance type eg. t2.micro, t2.medium
+  #instance_type = ""
+  #vpc_security_group_ids = [aws_security_group.web_ssh.id]
 #input a key-pair name if you'd like in ""
   #key_name = ""
+
+#
+resource "aws_vpc" "" {
+  cidr_block = "10.0.0.0/16"
+}
 
 #Input a file that will deploy Jenkins/vs code in user_data(make sure its a .sh script eg."deploy.sh") 
 user_data = "${file("")}"
@@ -24,4 +30,9 @@ user_data = "${file("")}"
     "Name" : "tf_practice"
   }
 
+}
+
+#outputs new instance IP in the terminal 
+output "instance_ip" {
+  value = aws_instance.web_server01.public_ip
 }
